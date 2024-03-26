e2ecadd20f6d7bd5adef23690784717c7b60e0da
michael@imick.io <michael.boutin@toptal.com>
2024-03-26 09:29:12 -0400

    Added a posts model

--- app.js
+++ app.js
@@ -3,12 +3,40 @@ const mongoose = require("mongoose");
 const { mongoUri, port } = require("./config");
 
 const userRoutes = require("./routes/users");
+const postRoutes = require("./routes/posts");
 const errorController = require("./controllers/error.controller");
+const User = require("./models/user");
 
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  User.findOne().then((user) => {
+    if (!user) {
+      const newUser = new User({
+        name: "John Doe",
+        email: "john@doe.com",
+      });
+
+      newUser
+        .save()
+        .then((result) => {
+          console.log("User created");
+          req.user = result;
+          next();
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } else {
+      req.user = user;
+      next();
+    }
+  });
+});
+
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 app.use(errorController.get404);
 
 mongoose
---
+++ controllers/post.controller.js
@@ -0,0 +1,37 @@
+const Post = require("../models/post");
+
+exports.all = async (req, res) => {
+  Post.find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+};
+
+exports.create = async (req, res) => {
+  const { title, content } = req.body;
+  const userId = req.user?._id;
+
+  if (!userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const post = new Post({
+    title,
+    content,
+    userId,
+  });
+
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+};
